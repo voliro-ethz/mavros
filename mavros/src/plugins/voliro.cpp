2e5352f1bc8c0951326760bb2296d04da30d5f2d
Omar Elkhatib <omar@voliro.ch>
2017-03-27 12:04:20 +0200

    Made support to receive only alphas from PX4

--- mavros/src/plugins/voliro.cpp
+++ mavros/src/plugins/voliro.cpp
@@ -1,5 +1,6 @@
 #include <mavros/mavros_plugin.h>
 #include <mavros_msgs/voliro_ao.h>
+#include <mavros_msgs/voliro_alpha.h>
 
 namespace mavros {
 namespace std_plugins {
@@ -12,7 +13,8 @@ class VOLIROPubPlugin : public plugin::PluginBase {
 public:
 	VOLIROPubPlugin() : PluginBase(),
 		volo_nh("~voliro"),
-		has_voliro(false)
+		has_voliro(false),
+		has_voliro_alpha(false)
 	{ }
 
 	void initialize(UAS &uas_)
@@ -20,6 +22,7 @@ public:
 		PluginBase::initialize(uas_);
 
 		voliro_pub = volo_nh.advertise<mavros_msgs::voliro_ao>("voliro_ao",10);
+		voliro_alpha_pub = volo_nh.advertise<mavros_msgs::voliro_alpha>("voliro_alpha",10);
 
 		// reset has_* flags on connection change
 		enable_connection_cb();
@@ -28,14 +31,17 @@ public:
 	Subscriptions get_subscriptions() {
 		return {
            make_handler(&VOLIROPubPlugin::handle_voliro),
+					 make_handler(&VOLIROPubPlugin::handle_voliro_alpha),
 		};
 	}
 
 private:
 	ros::NodeHandle volo_nh;
 	ros::Publisher voliro_pub;
+	ros::Publisher voliro_alpha_pub;
 
 	bool has_voliro;
+	bool has_voliro_alpha;
 
 	void handle_voliro(const mavlink::mavlink_message_t *msg, mavlink::common::msg::VOLIRO_AO &volao)
 	{
@@ -69,6 +75,26 @@ private:
 		// voliro_pub.publish(volomsg);
 
 
+	}
+
+	void handle_voliro_alpha(const mavlink::mavlink_message_t *msg, mavlink::common::msg::VOLIRO_ALPHA &vola)
+	{
+
+		ROS_INFO_COND_NAMED(!has_voliro_alpha, "voliro", "VOLIRO: VOLIRO ALPHA detected!");
+		has_voliro_alpha = true;
+
+		//ROS_INFO("VOLIRO WAS HERE");
+
+		mavros_msgs::voliro_alpha volo_alpha_msg;
+
+		volo_alpha_msg.alpha[0] = vola.alpha[0];
+		volo_alpha_msg.alpha[1] = vola.alpha[1];
+		volo_alpha_msg.alpha[2] = vola.alpha[2];
+		volo_alpha_msg.alpha[3] = vola.alpha[3];
+		volo_alpha_msg.alpha[4] = vola.alpha[4];
+		volo_alpha_msg.alpha[5] = vola.alpha[5];
+
+		voliro_alpha_pub.publish(volo_alpha_msg);
 	}
 
 	void connection_cb(bool connected) override
