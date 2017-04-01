f38bbc13448f331e265cb67655d9559128905874
Omar Elkhatib <omar@voliro.ch>
2017-04-01 15:26:44 +0200

    Added coordinate transform to voliro_full_setpoint message

--- mavros/src/plugins/voliro_waypoint.cpp
+++ mavros/src/plugins/voliro_waypoint.cpp
@@ -1,6 +1,8 @@
 #include <mavros/mavros_plugin.h>
 #include <geometry_msgs/PoseStamped.h>
 
+#include <eigen_conversions/eigen_msg.h>
+
 namespace mavros {
 namespace std_plugins{
 
@@ -30,18 +32,39 @@ private:
 	{
 		mavlink::common::msg::VOLIRO_FULL_SETPOINT v{};
 
-    v.time_boot_ms = sp->header.stamp.toNSec()/1000;
-    v.target_system = 1;
-    v.target_component = 1;
+		Eigen::Affine3d tr;
+		tf::poseMsgToEigen(sp->pose, tr);
 
-    v.q[0] = sp->pose.orientation.x;
-    v.q[1] = sp->pose.orientation.y;
-    v.q[2] = sp->pose.orientation.z;
-    v.q[3] = sp->pose.orientation.w;
+		// Transform quaternion and position to NED Coordinate Frame
 
-    v.x = sp->pose.position.x;
-    v.y = sp->pose.position.y;
-    v.z = sp->pose.position.z;
+		auto p = ftf::transform_frame_enu_ned(Eigen::Vector3d(tr.translation()));
+		auto q = ftf::transform_orientation_enu_ned(
+					ftf::transform_orientation_baselink_aircraft(Eigen::Quaterniond(tr.rotation())));
+
+		v.time_boot_ms = sp->header.stamp.toNSec() / 1000000;
+		v.target_system = 1;
+		v.target_component = 1;
+
+		v.x = p.x();
+		v.y = p.y();
+		v.z = p.z();
+
+		// Is this the correct order of quaternions?
+
+		v.q[0] = q.w();
+		v.q[1] = q.x();
+		v.q[2] = q.y();
+		v.q[3] = q.z();
+
+
+		// v.q[0] = sp->pose.orientation.x;
+		// v.q[1] = sp->pose.orientation.y;
+		// v.q[2] = sp->pose.orientation.z;
+		// v.q[3] = sp->pose.orientation.w;
+
+    // v.x = sp->pose.position.x;
+    // v.y = sp->pose.position.y;
+    // v.z = sp->pose.position.z;
 
 		UAS_FCU(m_uas)->send_message_ignore_drop(v);
 	}
