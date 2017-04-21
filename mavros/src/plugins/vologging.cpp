c4e4714d9966bf22fb514e4abbc036f0f6f4242d
Omar Elkhatib <omar@voliro.ch>
2017-04-21 14:26:56 +0200

    asj

--- mavros/src/plugins/vologging.cpp
+++ mavros/src/plugins/vologging.cpp
@@ -1,6 +1,6 @@
 #include <mavros/mavros_plugin.h>
-#include <mavros_msgs/voliro_alpha.h>
-#include <mavros_msgs/voliro_ao.h>
+// #include <mavros_msgs/voliro_alpha.h>
+// #include <mavros_msgs/voliro_ao.h>
 #include <mavros_msgs/vologging.h>
 
 namespace mavros {
@@ -49,69 +49,68 @@ void handle_vologging(const mavlink::mavlink_message_t   *msg,
                         "VOLIRO: VOLIRO LOGGING detected!");
     has_vologging = true;
 
-    mavros_msgs::vologging vologging;
+    mavros_msgs::vologging Vologging;
 
-    // ports for debbuging
-    vologging.header      = m_uas->synchronized_header(frame_id,
-                                                            logging.time_boot_ms);
+    // ports for debugging
+    Vologging.header = m_uas->synchronized_header(frame_id,  logging.time_boot_ms);
 		for(int i=0;i<3;++i){
-    vologging.port_A_1[i] = logging.port_A_1[i];
-    vologging.port_A_2[i] = logging.port_A_2[i];
-    vologging.port_A_3[i] = logging.port_A_3[i];
-    vologging.port_A_4[i] = logging.port_A_4[i];
-    vologging.port_A_5[i] = logging.port_A_5[i];
-    vologging.port_A_6[i] = logging.port_A_6[i];
-    vologging.port_A_7[i] = logging.port_A_7[i];
-    vologging.port_A_8[i] = logging.port_A_8[i];
-    vologging.port_A_9[i] = logging.port_A_9[i];
-    vologging.port_B_1[i] = logging.port_B_1[i];
-    vologging.port_B_2[i] = logging.port_B_2[i];
-    vologging.port_B_3[i] = logging.port_B_3[i];
-    vologging.port_B_4[i] = logging.port_B_4[i];
-    vologging.port_B_5[i] = logging.port_B_5[i];
-    vologging.port_B_6[i] = logging.port_B_6[i];
-    vologging.port_B_7[i] = logging.port_B_7[i];
-    vologging.port_B_8[i] = logging.port_B_8[i];
-    vologging.port_B_9[i] = logging.port_B_9[i];
-    /*vologging.port_C_1[i] = logging.port_C_1[i];
-    vologging.port_C_2[i] = logging.port_C_2[i];
-    vologging.port_C_3[i] = logging.port_C_3[i];
-    vologging.port_C_4[i] = logging.port_C_4[i];
-    vologging.port_C_5[i] = logging.port_C_5[i];
-    vologging.port_C_6[i] = logging.port_C_6[i];
-    vologging.port_C_7[i] = logging.port_C_7[i];
-    vologging.port_C_8[i] = logging.port_C_8[i];
-    vologging.port_C_9[i] = logging.port_C_9[i];
-    vologging.port_D_1[i] = logging.port_D_1[i];
-    vologging.port_D_2[i] = logging.port_D_2[i];
-    vologging.port_D_3[i] = logging.port_D_3[i];
-    vologging.port_D_4[i] = logging.port_D_4[i];
-    vologging.port_D_5[i] = logging.port_D_5[i];
-    vologging.port_D_6[i] = logging.port_D_6[i];
-    vologging.port_D_7[i] = logging.port_D_7[i];
-    vologging.port_D_8[i] = logging.port_D_8[i];
-    vologging.port_D_9[i] = logging.port_D_9[i];*/
+    Vologging.port_A_1[i] = logging.port_A_1[i];
+    Vologging.port_A_2[i] = logging.port_A_2[i];
+    Vologging.port_A_3[i] = logging.port_A_3[i];
+    Vologging.port_A_4[i] = logging.port_A_4[i];
+    Vologging.port_A_5[i] = logging.port_A_5[i];
+    Vologging.port_A_6[i] = logging.port_A_6[i];
+    Vologging.port_A_7[i] = logging.port_A_7[i];
+    Vologging.port_A_8[i] = logging.port_A_8[i];
+    Vologging.port_A_9[i] = logging.port_A_9[i];
+    Vologging.port_B_1[i] = logging.port_B_1[i];
+    Vologging.port_B_2[i] = logging.port_B_2[i];
+    Vologging.port_B_3[i] = logging.port_B_3[i];
+    Vologging.port_B_4[i] = logging.port_B_4[i];
+    Vologging.port_B_5[i] = logging.port_B_5[i];
+    Vologging.port_B_6[i] = logging.port_B_6[i];
+    Vologging.port_B_7[i] = logging.port_B_7[i];
+    Vologging.port_B_8[i] = logging.port_B_8[i];
+    Vologging.port_B_9[i] = logging.port_B_9[i];
+    /*Vologging.port_C_1[i] = logging.port_C_1[i];
+    Vologging.port_C_2[i] = logging.port_C_2[i];
+    Vologging.port_C_3[i] = logging.port_C_3[i];
+    Vologging.port_C_4[i] = logging.port_C_4[i];
+    Vologging.port_C_5[i] = logging.port_C_5[i];
+    Vologging.port_C_6[i] = logging.port_C_6[i];
+    Vologging.port_C_7[i] = logging.port_C_7[i];
+    Vologging.port_C_8[i] = logging.port_C_8[i];
+    Vologging.port_C_9[i] = logging.port_C_9[i];
+    Vologging.port_D_1[i] = logging.port_D_1[i];
+    Vologging.port_D_2[i] = logging.port_D_2[i];
+    Vologging.port_D_3[i] = logging.port_D_3[i];
+    Vologging.port_D_4[i] = logging.port_D_4[i];
+    Vologging.port_D_5[i] = logging.port_D_5[i];
+    Vologging.port_D_6[i] = logging.port_D_6[i];
+    Vologging.port_D_7[i] = logging.port_D_7[i];
+    Vologging.port_D_8[i] = logging.port_D_8[i];
+    Vologging.port_D_9[i] = logging.port_D_9[i];*/
 }
-vologging.port_A_1[3] = 0;//Last component doesn'exist in the mavlink message
-vologging.port_A_2[3] = 0;
-vologging.port_A_3[3] = 0;
-vologging.port_A_4[3] = 0;
-vologging.port_A_5[3] = 0;
-vologging.port_A_6[3] = 0;
-vologging.port_A_7[3] = 0;
-vologging.port_A_8[3] = 0;
-vologging.port_A_9[3] = 0;
-vologging.port_B_1[3] = 0;
-vologging.port_B_2[3] = 0;
-vologging.port_B_3[3] = 0;
-vologging.port_B_4[3] = 0;
-vologging.port_B_5[3] = 0;
-vologging.port_B_6[3] = 0;
-vologging.port_B_7[3] = 0;
-vologging.port_B_8[3] = 0;
-vologging.port_B_9[3] = 0;
+Vologging.port_A_1[3] = 0;//Last component doesn'exist in the mavlink message
+Vologging.port_A_2[3] = 0;
+Vologging.port_A_3[3] = 0;
+Vologging.port_A_4[3] = 0;
+Vologging.port_A_5[3] = 0;
+Vologging.port_A_6[3] = 0;
+Vologging.port_A_7[3] = 0;
+Vologging.port_A_8[3] = 0;
+Vologging.port_A_9[3] = 0;
+Vologging.port_B_1[3] = 0;
+Vologging.port_B_2[3] = 0;
+Vologging.port_B_3[3] = 0;
+Vologging.port_B_4[3] = 0;
+Vologging.port_B_5[3] = 0;
+Vologging.port_B_6[3] = 0;
+Vologging.port_B_7[3] = 0;
+Vologging.port_B_8[3] = 0;
+Vologging.port_B_9[3] = 0;
 
-    vologging_pub.publish(vologging);
+    vologging_pub.publish(Vologging);
   }
 
   void connection_cb(bool connected) override
