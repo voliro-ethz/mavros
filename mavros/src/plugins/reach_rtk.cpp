b3257da50275d4b92041a5c017f094d345353095
Omar Elkhatib <omar@voliro.ch>
2017-05-15 11:47:07 +0200

    Added Voliro GPS Plugin for Reach

--- mavros/src/plugins/reach_rtk.cpp
+++ mavros/src/plugins/reach_rtk.cpp
@@ -9,17 +9,17 @@
 namespace mavros {
 namespace std_plugins{
 
-class VOLWaypointPlugin : public plugin::PluginBase
+class VOLReachPlugin : public plugin::PluginBase
 {
 public:
-	VOLWaypointPlugin() : PluginBase(),
+	VOLReachPlugin() : PluginBase(),
 		voliro_nh("~voliro")
 	{ }
 
 	void initialize(UAS &uas_)
 	{
 		PluginBase::initialize(uas_);
-		full_gps_sub = voliro_nh.subscribe("gps_fix", 1, &VOLWaypointPlugin::voliro_full_gps_cb, this);
+		full_gps_sub = voliro_nh.subscribe("full_gps", 1, &VOLReachPlugin::voliro_full_gps_cb, this);
 	}
 
 	Subscriptions get_subscriptions()
@@ -33,9 +33,38 @@ private:
 
 	void voliro_full_gps_cb(const mavros_msgs::full_gps::ConstPtr &raw_gps) {
 
-    mavlink::common::msg::GPS_RAW_INT gps;
-    gps.fix = ra
+		mavlink::common::msg VOLIRO_GPS gps_out;
+		gps_out.time_usec = raw_gps->fix.header.stamp.toNSec() / 1000;
+		gps_out.fix_type = raw_gps->fix.status.status + 1;
+		gps_out.lat = raw_gps->fix.latitude * 10000000; // [degrees * 1E7]
+		gps_out.lon = raw_gps->fix.longitude * 10000000; // [degrees * 1E7]
+		gps_out.alt = raw_gps->fix.altitude * 1000; // [m * 1000] AMSL
 
+		// ??? TODO
+		gps_out.eph = 1.0f;
+		gps_out.epv = 1.0f;
+
+		// Velocities ! -> Transform
+
+		//  Verify all the Math !!!! TODO
+
+		// Transform velocities to NED   -> Important to note the velocity part
+		auto velocity = ftf::transform_frame_enu_ned(Eigen::Vector3d(
+						raw_gps->vel.linear.x,
+						raw_gps->vel.linear.y,
+						raw_gps->vel.linear.z));
+
+		double vn = velocity.x();
+		double ve = velocity.y();
+		double vd = velocity.z();
+
+		gps_out.vel = sqrt(vn * vn + ve * ve);  // [m/s] !!
+		gps_out.vn = vn; 												// [m/s] !!
+		gps_out.ve = ve; 												// [m/s] !!
+		gps_out.vd = vd; 												// [m/s] !!
+
+		// Hardcode
+		gps_out.satellites_visible = 5;
 
 		UAS_FCU(m_uas)->send_message_ignore_drop(v);
 	}
@@ -45,4 +74,4 @@ private:
 }	// namespace mavros
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(mavros::std_plugins::VOLWaypointPlugin, mavros::plugin::PluginBase)
+PLUGINLIB_EXPORT_CLASS(mavros::std_plugins::VOLReachPlugin, mavros::plugin::PluginBase)
